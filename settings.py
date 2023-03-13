326c5a1c53e618e7300a8bf8c4aa77269c9cc66b
benpfaff23 <pfaffben23@gmail.com>
2023-03-13 11:26:30 -0600

    plz commit

---
+++ settings.py
@@ -0,0 +1,22 @@
+# File created by Ben Pfaff
+
+# screen color  
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255,50,50)
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+# player attributes 
+PLAYER_ACC = 2 
+PLAYER_FRICTION = -0.12
+
+# mob attritubutes
+MOB_ACC = 2
+MOB_FRICTION = -0.12
