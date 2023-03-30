fa6a548d3855a6a9fe4a754828ed16fe9417546c
benpfaff23 <pfaffben23@gmail.com>
2023-03-30 14:25:26 -0700

    3/30/23

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ BLACK = (0,0,0)
 BLUE = (50,50,255)
 RED = (255,50,50)
 WHITE = (255,255,255)
-
+GREEN = (0,255,0)
 # screen dimensions
 WIDTH = 800
 HEIGHT = 600
@@ -17,7 +17,19 @@ RUNNING = True
 # player attributes 
 PLAYER_ACC = 2 
 PLAYER_FRICTION = -0.12
+PLAYER_GRAV = .8
+PLAYER_JUMP = 20
 
 # mob attritubutes
 MOB_ACC = 2
 MOB_FRICTION = -0.12
+
+PAUSED = False 
+SCORE = 0
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (100,255,100), "bouncey"),
+                 (125, HEIGHT - 350, 100, 5, (200,100,50), "disappearing"),
+                 (350, 200, 100, 20, (200,200,200), "normal"),
+                 (175, 100, 50, 20, (200,200,200), "normal")]
